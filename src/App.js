68f5ba10faef365793d1a756d2b8545cf160d44c
Jimmy Svensson <jimmy888swe@gmail.com>
2020-03-05 23:13:36 +0100

    feat: added imports and routes for new components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import  HeaderComponent  from './components/HeaderComponent';
 import Auth from './page/Auth';
 import  Dashboard  from './page/Dashboard';
+import Task from './page/Task';
+import Stats from './page/Stats';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
@@ -12,6 +14,8 @@ function App() {
       <HeaderComponent></HeaderComponent>
       <Route path="/" exact component={Auth} />
       <Route path="/dashboard" component={Dashboard} />
+      <Route path="/task" component={Task} />
+      <Route path="/stats" component={Stats} />
     </Router>
   );
 }
