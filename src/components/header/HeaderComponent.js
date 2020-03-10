19114687f12944994cd0ee2c92a901d63e19fb91
Jimmy Svensson <jimmy888swe@gmail.com>
2020-03-10 22:14:49 +0100

    feat: headerComponent with linkts to diff paths

--- src/components/header/HeaderComponent.js
+++ src/components/header/HeaderComponent.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './header.css';
+import { Link } from 'react-router-dom';
 
 export default function HeaderComponent() {
   const street = 'Legendvägen 36'
   return (
-    <div className="header">
-      <h1 className="street">{street}</h1>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <Link to="/dashboard" className="navbar-brand" >
+      {street}
+    </Link>
+      <div className="collpase nav-collpase">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+            <Link to="/dashboard" className="nav-link">Dashboard</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/stats" className="nav-link">Stats</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/task" className="nav-link">Task</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
