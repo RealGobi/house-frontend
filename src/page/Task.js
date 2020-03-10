c51f1684355c374952f0ab38f4cc90f99f033c5a
Jimmy Svensson <jimmy888swe@gmail.com>
2020-03-10 22:57:41 +0100

    feat: hooks to control basic form for now

--- src/page/Task.js
+++ src/page/Task.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 export default function Task() {
+  const [title, setTitle] = useState('');
+  const [step, setStep] = useState('');
+  const [desc, setDesc] = useState('');
+
+  const resetForm = () => {
+    setTitle('');
+    setStep('');
+    setDesc('')
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Form Submited');
+    console.log('Title: ', title);
+    console.log('Step: ', step);
+    console.log('Desc: ', desc);
+    resetForm();
+  }
+
+
   return (
     <div>
       <h1>Task comp</h1>
