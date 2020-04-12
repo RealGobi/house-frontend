1624e178f3a1a656e2f558db44a3ec772a3fff58
Jimmy Svensson <jimmy888swe@gmail.com>
2020-04-12 23:06:09 +0200

    feat axios added

--- src/context/TaskContext.js
+++ src/context/TaskContext.js
@@ -1,12 +1,21 @@
 import React, { useState, createContext } from 'react';
+import axios from 'axios';
 
 export const TaskContext = createContext();
 
 export const TaskProvider = (props) => {
 
   const [task, setTask] = useState();
+  const URL = 'http://localhost:4000';
+
+  axios.get(URL + '/task')
+  .then(res => {
+    const tasks = res.data;
+    setTask(tasks)
+    });
+
   return(
-    <TaskContext.Provider value={'jimgwrtgergqergqrgqemy'}>
+    <TaskContext.Provider value={task}>
       {props.children}
     </TaskContext.Provider>
   );
