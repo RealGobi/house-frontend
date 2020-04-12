3b515ec9fde669c223bc1557d4840f53ac4946d7
Jimmy Svensson <jimmy888swe@gmail.com>
2020-04-12 23:07:24 +0200

    feat: first draft of context in dash

--- src/page/Dashboard.js
+++ src/page/Dashboard.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { TaskContext } from '../context/TaskContext';
 
 export default function Dashboard() {
+  const task = useContext(TaskContext);
+console.log(task);
+
+  
   return (
-    <div>
+    <div className="col">
       <h1>Dashboard comp</h1>
+      <div>
+
+      </div>
     </div>
   )
 }
