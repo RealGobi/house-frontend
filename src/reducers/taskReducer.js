55905311b83593c2fa9e507b008bc1fc800896af
Jimmy Svensson <jimmy888swe@gmail.com>
2020-04-16 23:17:29 +0200

    feat: task reducer

---
+++ src/reducers/taskReducer.js
@@ -0,0 +1,11 @@
+import { GET_TASKS, ADD_TASK, DELETE_TASK } from '../actions/types'
+
+const initialState = {
+  tasks: [
+
+  ]
+}
+
+export default function (state = initialState, actions) {
+  
+}
