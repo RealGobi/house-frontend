1714fcf16c253d7fe3f5c8356401ae6a8e3efa8c
Jimmy Svensson <jimmy888swe@gmail.com>
2020-04-18 00:02:17 +0200

    fix: name change

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { compose, applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './reducer';
+import rootReducer from './reducers';
 
 const initiakState = {};
 
