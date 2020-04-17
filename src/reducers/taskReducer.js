410c3144dd695bca634d688faa39932000e7b6ad
Jimmy Svensson <jimmy888swe@gmail.com>
2020-04-18 00:00:41 +0200

    feat: task reducer

--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -1,11 +1,20 @@
-import { GET_TASKS, ADD_TASK, DELETE_TASK } from '../actions/types'
+import { GET_TASKS, ADD_TASK, DELETE_TASK } from '../actions/types';
 
 const initialState = {
   tasks: [
-
+    {name: 'GRÄVA'},
+    {name: 'SKRUVA'},
+    {name: 'GRÅTA'}
   ]
 }
 
 export default function (state = initialState, actions) {
-  
+  switch(actions.type){
+    case GET_TASKS:
+      return {
+        ...state
+      }
+      default:
+        return state;
+  }
 }
