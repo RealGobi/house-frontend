e46c2d3a73bb947707faf1f41d16e32f17b299eb
Jimmy Svensson <jimmy888swe@gmail.com>
2021-01-20 20:25:00 +0100

    feat: Types for redux Auth,Err

--- src/actions/types.js
+++ src/actions/types.js
@@ -2,3 +2,15 @@ export const GET_TASKS = 'GET_TASKS';
 export const ADD_TASK = 'ADD_TASK';
 export const DELETE_TASK = 'DELETE_TASK';
 export const LOADING_TASK = 'LOADING_TASK';
+
+
+export const USER_LOADING = 'USER_LOADING';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCSSES = 'LOGIN_SUCSSES';
+export const LOGOUT_SUCSSES = 'LOGOUT_SUCSSES';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const REGISTER_SUCSSES = 'REGISTER_SUCSSES';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const GET_ERROR = 'GET_ERROR';
+export const CLEAR_ERROR = 'CLEAR_ERROR';
