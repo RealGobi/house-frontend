4973644f62e6319e0735923a574b918ec8812057
Jimmy Svensson <jimmy888swe@gmail.com>
2021-01-22 00:31:49 +0100

    fix

--- src/page/Auth.js
+++ src/page/Auth.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Registration from '../components/auth/Registration';
 
 export default function Auth() {
   return (
     <div>
       <h1>Auth comp</h1>
+      <Registration />
     </div>
   )
 }
